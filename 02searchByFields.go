0bbc37d2f87826ab2c01d35f731c39fd24bbbd48
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-04-17 12:06:29 +0530

    Terminal Search By Specific Field Names

--- 02searchByFields.go
+++ 02searchByFields.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 
-func searchStudent(inputEntry, colmName string, index int, studentId int64) ([]StudentData, error) {
+func searchStudent(searchkey, colmName string, index int, studentId int64) ([]StudentData, error) {
 	var students []StudentData
 	var err error
 	var rows *sql.Rows
 
 	if index == 1 {
-		rows, err = db.Query("SELECT * FROM student_Data WHERE fatherName = ?", inputEntry)
+		rows, err = db.Query("SELECT * FROM student_Data WHERE fatherName = ?", searchkey)
 	} else if index == 2 {
-		rows, err = db.Query("SELECT * FROM student_Data WHERE motherName = ?", inputEntry)
+		rows, err = db.Query("SELECT * FROM student_Data WHERE motherName = ?", searchkey)
 	} else if index == 3 {
-		rows, err = db.Query("SELECT * FROM student_Data WHERE studentName = ?", inputEntry)
+		rows, err = db.Query("SELECT * FROM student_Data WHERE studentName = ?", searchkey)
 	} else if index == 5 {
-		rows, err = db.Query("SELECT * FROM student_Data WHERE city = ?", inputEntry)
+		rows, err = db.Query("SELECT * FROM student_Data WHERE city = ?", searchkey)
 	} else if index == 6 {
 		rows, err = db.Query("SELECT * FROM student_Data WHERE studentId = ?", studentId)
 	} else if index == 7 {
@@ -25,19 +25,19 @@ func searchStudent(inputEntry, colmName string, index int, studentId int64) ([]S
 	}
 
 	if err != nil {
-		return nil, fmt.Errorf("error1: no student found with %s = %s, %v,  ", colmName, inputEntry, err)
+		return nil, fmt.Errorf("error1: no student found with %s = %s, %v,  ", colmName, searchkey, err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
 		var alb StudentData
 		if err := rows.Scan(&alb.StudentId, &alb.StudentName, &alb.FatherName, &alb.MotherName, &alb.Cgpa, &alb.City); err != nil {
-			return nil, fmt.Errorf("error2: no student found with %s = %s, %v,  ", colmName, inputEntry, err)
+			return nil, fmt.Errorf("error2: no student found with %s = %s, %v,  ", colmName, searchkey, err)
 		}
 		students = append(students, alb)
 	}
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("error3 : no student found with %s = %s, %v,  ", colmName, inputEntry, err)
+		return nil, fmt.Errorf("error3 : no student found with %s = %s, %v,  ", colmName, searchkey, err)
 	}
 	return students, nil
 }
